f60ea8a854e4207f6e57a26b3af8b16ff3c648fd
Erik Martín Jordán <erikmartinjordan@gmail.com>
2021-01-13 14:18:06 +0100

    Added fmpeg static

--- src/server.js
+++ src/server.js
@@ -1,35 +1,26 @@
-const express = require('express');
-Stream        = require('node-rtsp-stream');
+const express     = require('express');
+const ffmpegPath  = require('ffmpeg-static');
+Stream            = require('node-rtsp-stream');
 
 let app = express();
 
 let server = app.listen(3000);
 
-app.get('/', function(req, res){
-    
-    res.send('Hola! El servidor està on');
-    
-});
-
-app.get('/test', function(req, res){
-   
-    res.send('Hola! Això és un test');
-    
-});
-
+// Es crea un servidor que escolta el port 3000
+// S'envia un flux de streaming per al port 9000
+// Per executar aquesta funció -> https://localhost:3000
 app.get('/video', function(req, res){
     
     stream = new Stream({
         
         name: 'name',
         streamUrl: 'rtsp://184.72.239.149/vod/mp4:BigBuckBunny_115k.mov',
-        wsPort: 9000
+        wsPort: 9000,
+        ffmpegPath: ffmpegPath
         
     });
-    
-    res.send(stream);
    
-    /*res.send(
+    res.send(
         `<!DOCTYPE html>
             <html>
             <head>
@@ -55,6 +46,6 @@ app.get('/video', function(req, res){
                 </script>
             </body>
             </html>`
-    );*/
+    );
     
 });

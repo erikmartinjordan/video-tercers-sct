41a2d5aca9743f0f5c157ff7ef5304a88ae28b44
Erik Martín Jordán <erikmartinjordan@gmail.com>
2021-01-14 10:47:05 +0100

    Added function to launch VLC

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
+const { spawn }                      = require('child_process');
 
 require('electron-reload')();
 
@@ -16,7 +17,39 @@ function createWindow () {
     
 }
 
-app.whenReady().then(createWindow);
+function activateCamera (num_camera) {
+    
+    
+}
+
+function deactivateCamera (num_camera) {
+    
+    
+    
+}
+
+function launchVLC (rtsp_link) {
+    
+    const path = 'C:\\Program Files\\VideoLAN\\VLC\\vls.exe';
+     
+    const proc = spawn(`"${path}"`, [`http://46.16.226.181:88/?action=stream`], { shell: true });
+
+    proc.stdout.on('data', function (data) {
+        
+        const options = {
+            
+            'title': 'Advertència',
+            'message': 'Si us plau, instal·la VLC al teu ordinador'
+            
+        }
+        
+        console.log(dialog.showMessageBox(null, options));
+        
+    });
+
+}
+
+app.whenReady().then(createWindow).then(launchVLC);
 
 app.on('window-all-closed', () => {
     

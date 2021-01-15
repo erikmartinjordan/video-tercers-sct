987416580bf3539d64fcef432596c14912cb9feb
Erik Martín Jordán <erikmartinjordan@gmail.com>
2021-01-15 11:26:24 +0100

    Hide login if login was correct

--- src/login.js
+++ src/login.js
@@ -1,19 +1,39 @@
+import config from './config.js';
+
 const Login = () => {
     
+    const [auth, setAuth] = React.useState(false);
     const [user, setUser] = React.useState(null);
     const [pass, setPass] = React.useState(null);
+    
+    const validate = async () => {
+        
+        let { address, port } = config[user];
+        
+        let res = await fetch(`http://${user}:${password}@${address}:${port}/set?operation=status`);
+        
+        if(res.ok){
+            
+            setAuth(true);
+            
+        }
+        
+    }
 
     return(
         <React.Fragment>
-            <div className = 'Login'>
+            { !auth
+            ? <div className = 'Login'>
                 <div className = 'Login-Wrapper'>
                     <img src = {'./assets/sct_logo.png'}></img>
                     <p>Si us plau, identifíca't per poder utilitzar l'aplicació de vídeo a tercers del Servei Català del Trànsit:</p>
                     <input onChange = {(e) => setUser(e.target.value)} placeholder = 'Usuari'></input>
                     <input onChange = {(e) => setPass(e.target.value)} placeholder = 'Contrasenya'></input>
-                    <button>Login</button>
+                    <button onClick = {validate}>Accedeix</button>
                 </div>
-            </div>
+              </div>
+            : null                
+            }
         </React.Fragment>
     );
     

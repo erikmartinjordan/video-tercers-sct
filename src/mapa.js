6bab45a3144c5b152a73daab7106cc2cf3a914d6
Erik Martín Jordán <erikmartinjordan@gmail.com>
2021-01-18 15:08:41 +0100

    Added zoom control on the right bottom of the page and reset zoom

--- src/mapa.js
+++ src/mapa.js
@@ -4,7 +4,10 @@ import cameres from './src/cameres.js';
 var element = document.getElementById('mapa-catalunya');
 
 // Afegir Leaflet al mapa
-window.mapa = L.map(element);
+window.mapa = L.map(element, {zoomControl: false});
+
+// Afegir control de zoom a la part dreta inferior de la pàgina
+L.control.zoom({position: 'bottomright'}).addTo(mapa);
 
 // Afegir capes (tiles) OSM
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
@@ -20,4 +23,4 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 var marcador = L.latLng('41.39576618797968', '2.173775823753265');
 
 // Seleccionem el marcador amb zoom de 25
-mapa.setView(marcador, 25);
+mapa.setView(marcador, 10);

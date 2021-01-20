fb9c521c299e719619c311749da68a062580d9b9
Erik Martín Jordán <erikmartinjordan@gmail.com>
2021-01-20 12:15:54 +0100

    Finished search component

--- src/search.js
+++ src/search.js
@@ -2,17 +2,33 @@ import cameres from './src/cameres.js';
 
 const Search = () => {
     
-    const [numero, setNumero] = React.useState(null);
+    const [numero, setNumero] = React.useState('');
 
+    const centerMap = (id) => {
+        
+        var lat = cameres[id].coordenades.lat;
+        var lng = cameres[id].coordenades.lng;
+        
+        var marcador = L.latLng(lat, lng);
+
+        mapa.setView(marcador, 25);
+        
+        setNumero('');
+        
+    }
+    
     return(
         <div className = 'Cerca'>
-            <input placeholder = 'Introdueix el número de càmera...' onChange = {(e) => setNumero(e.target.value)}></input>
+            <input placeholder = 'Introdueix el número de càmera...' onChange = {(e) => setNumero(e.target.value)} value = {numero}></input>
             { numero
             ? <div className = 'Llistat'>
-                {Object.keys(cameres).filter(camera => camera.startsWith(numero)).map((numero, key) => 
-                    <li key = {key}>
-                        <div className = 'Carretera'>📍 {cameres[numero].carretera}</div>
-                        <div className = 'Pk'>(Pk {cameres[numero].pk})</div>
+                {Object.keys(cameres).filter(camera => camera.startsWith(numero)).map((id, key) => 
+                    <li key = {key} onClick = {() => centerMap(id)}>
+                        <div className = 'Carretera-Pk'>
+                            <span className = 'Carretera'>📍 {cameres[id].carretera}</span>
+                            <span className = 'Pk'>(Pk {cameres[id].pk})</span>
+                        </div>
+                        <div className = 'Camera'>{id}</div>                                                                 
                     </li>
                 )}
               </div>

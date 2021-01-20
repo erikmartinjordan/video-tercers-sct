00410aeccf0ddd0d9c17e9407ac48616d979856d
Erik Martín Jordán <erikmartinjordan@gmail.com>
2021-01-20 12:16:18 +0100

    Reset initial state of the map

--- src/mapa.js
+++ src/mapa.js
@@ -1,15 +1,11 @@
 import cameres from './src/cameres.js';
 
-// Agafar la referència del mapa de Catalunya
 var element = document.getElementById('mapa-catalunya');
 
-// Afegir Leaflet al mapa
 window.mapa = L.map(element, {zoomControl: false});
 
-// Afegir control de zoom a la part dreta inferior de la pàgina
 L.control.zoom({position: 'bottomright'}).addTo(mapa);
 
-// Afegir capes (tiles) OSM
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
@@ -19,8 +15,7 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1
 }).addTo(mapa);
 
-// Referència central del mapa 
+
 var marcador = L.latLng('41.39576618797968', '2.173775823753265');
 
-// Seleccionem el marcador amb zoom de 25
-mapa.setView(marcador, 10);
+mapa.setView(marcador, 13);

d73e48e821593b17917ed1f1e20c15463a12b04e
Erik Martín Jordán <erikmartinjordan@gmail.com>
2021-01-27 08:42:53 +0100

    Added user profile pics

--- src/config.js
+++ src/config.js
@@ -5,28 +5,28 @@ export default {
         "port": 81,
         "rtsp_ports": {
             "9001": {
-                "name": "Jean Peña",
-                "pic": "./assets/jean.png"
-            },
-            "9002": {
-                "name": "Lluís Serrano",
-                "pic": "./assets/lluis.png"
-            },
-            "9003": {
                 "name": "José Carlos García",
                 "pic": "./assets/carlos.png"
             },
-            "9004": {
+            "9002": {
                 "name": "Jordi Moli",
                 "pic": "./assets/jordi.png"
             },
-            "9005": {
+            "9003": {
+                "name": "Lluís Serrano",
+                "pic": "./assets/lluis.png"
+            },
+            "9004": {
                 "name": "Vicente Gallego",
                 "pic": "./assets/vicente.png"
             },
+            "9005": {
+                "name": "Jean Peña",
+                "pic": "./assets/jean.png"
+            },
             "9006": {
                 "name": "Visitant",
-                "pic": "./assets/visitant_1.png"
+                "pic": "./assets/visitant.png"
             }
         }
     }

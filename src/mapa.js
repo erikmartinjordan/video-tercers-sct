5e4c3bb467ff2a80c3355cb61a02209264aefd77
Erik Martín Jordán <erikmartinjordan@gmail.com>
2021-02-01 09:01:23 +0100

    Added ortophoto layer

--- src/mapa.js
+++ src/mapa.js
@@ -2,19 +2,24 @@ import cameres from './src/cameres.js';
 
 var element = document.getElementById('mapa-catalunya');
 
-window.mapa = L.map(element, {zoomControl: false});
+var mapboxURL = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw';
+
+var streets   = L.tileLayer(mapboxURL, {id: 'mapbox/streets-v11', tileSize: 512, zoomOffset: -1, attribution: 'Mapbox'});
+var ortophoto = L.tileLayer(mapboxURL, {id: 'mapbox/satellite-streets-v11', tileSize: 512, zoomOffset: -1, attribution: 'Mapbox'});
+
+window.mapa = L.map(element, {
+    zoomControl: false,
+    layers: [streets, ortophoto]
+});
+
+var baseMaps = {
+    "Ortophoto": ortophoto,
+    "Streets": streets
+}
+
+L.control.layers(baseMaps).addTo(mapa);
 
 L.control.zoom({position: 'bottomright'}).addTo(mapa);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1
-}).addTo(mapa);
-
 
 var marcador = L.latLng('41.39576618797968', '2.173775823753265');
 
